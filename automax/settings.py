5d30fd54413ded01db77cd5f8a98538cce129037
rounak <rounakrunwal08@gmail.com>
2024-06-28 12:18:05 +0530

    CSRF UPDATE

--- automax/settings.py
+++ automax/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DJANGO_DEBUG")
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "web-production-1991.up.railway.app"]
-CSRF_TRUTED_ORIGIN=["web-production-1991.up.railway.app"]
+CSRF_TRUTED_ORIGIN=["https://web-production-1991.up.railway.app"]
 
 
 # Application definition
